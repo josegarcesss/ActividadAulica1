873a2a33bd11b8f5a77614b5514a1e7fd3c5703f
facuulp <163307592+facuulp@users.noreply.github.com>
2024-03-21 16:09:23 -0300

    Creación de menú

--- src/aa1/AA1.java
+++ src/aa1/AA1.java
@@ -10,6 +10,16 @@ import java.util.Scanner;
 public class AA1 {
         //@param args the command line arguments
     public static void main(String[] args) {
+        Baraja baraja= new Baraja();
+        System.out.println("*barajando...");baraja.barajar();
+        System.out.println("*Cantidad de cartas restante: "+baraja.cartasDisponibles());
+        System.out.println("*mostrando la siguiente carta: "+baraja.SiguienteCarta());
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("*cuantas cartas desea sacar: ");
+        baraja.darCartas(scanner.nextInt());
+        
+        baraja.cartasMonton();
+        baraja.mostrarbaraja();
+    }
 
-}
 }
