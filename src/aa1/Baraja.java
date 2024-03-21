5ba6ad340c8b3d725ca3d3b6ab7a380ee0187a4e
Lauta2 <163307124+Lauta2@users.noreply.github.com>
2024-03-21 11:46:53 -0300

    Se agrego el metodo siguiente carta

--- src/aa1/Baraja.java
+++ src/aa1/Baraja.java
@@ -9,11 +9,18 @@ import java.util.Collections;
 
 public class Baraja {
     private final ArrayList<Carta> maso=new ArrayList<>();
-    private final int cartasEnBaraja;
+    private final ArrayList<Carta> monton=new ArrayList<>();
 
     public Baraja() {
         rellenar();
-        cartasEnBaraja=40;
+    }
+    
+    public int cartasDisponibles(){
+        return(maso.size());
+    }
+    
+    public int cartasDisponibles(){
+        return(maso.size());
     }
         
         
@@ -34,8 +41,43 @@ public class Baraja {
          Collections.shuffle(maso);
      }  
     
+<<<<<<< HEAD
+    public Carta SiguienteCarta(){
     
+
+        if(maso.isEmpty()) {
+            System.out.println("Baraja sin cartas");
+            return null;
+        }
+        Carta carta=maso.remove(0);
+        monton.add(carta);
+        return carta;
     
+    }
+    
+
+=======
+        public void cartasMonton() {
+        if (maso.size() == 40) {
+            System.out.println("Aun no se saco ninguna carta");
+        } else {
+            System.out.println("Cartas en el monton:");
+            for (Carta carta : maso) {
+                System.out.println("-"+carta);
+            }
+        }
+    }
+>>>>>>> 499c65161713e9733ff8dde65b7803cd101c0230
+    public void mostrarbaraja(){
+          System.out.println("Cartas en la baraja:");
+        for (Carta carta : maso) {
+            System.out.println("-"+carta);
+}
+}
+<<<<<<< HEAD
+
+=======
+>>>>>>> 499c65161713e9733ff8dde65b7803cd101c0230
 }
 
  
