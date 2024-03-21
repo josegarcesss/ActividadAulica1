8e4d3af9cae6b449bd6669ef079c7fad19ea7102
josegarcesss <joseggarces@protonmail.com>
2024-03-21 09:52:57 -0300

    Initial commit

---
+++ src/aa1/Carta.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aa1;
+
+/**
+ *
+ * @author Alakyan
+ */
+public class Carta {
+    private int numero;
+    private String  palo;
+
+    public Carta(int numero, String palo) {
+        this.numero = numero;
+        this.palo = palo;
+    }
+    
+    
+    
+    
+    
+}
